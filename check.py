18d186f639c4aafc8624d5cb4a7bd7c370625858
Dagmawi <dagmmike1224@gmail.com>
2026-09-26 12:06:51 +0300

    h

--- check.py
+++ check.py
@@ -39,3 +39,28 @@ do
 
     echo
 done
+
+
+
+python3 -c '
+import paramiko
+
+hosts = [
+    "10.10.10.200", "10.10.10.210", "10.10.10.10", "10.10.10.20",
+    "192.168.10.101", "192.168.20.100", "192.168.30.101", "192.168.10.102",
+    "192.168.20.210", "192.168.30.210"
+]
+
+for ip in hosts:
+    ssh = paramiko.SSHClient()
+    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    try:
+        ssh.connect(ip, port=22, username="ubuntu", password="ubuntu", timeout=3)
+        _, stdout, _ = ssh.exec_command("hostname; resolvectl dns")
+        out = stdout.read().decode().strip()
+        print(f"[{ip}] ->\n{out}\n" + "-"*35)
+    except Exception as e:
+        print(f"[{ip}] ERROR: {e}\n" + "-"*35)
+    finally:
+        ssh.close()
+'
